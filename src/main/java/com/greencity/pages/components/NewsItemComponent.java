64278be384e678d6a5b44052b60cfac71edfc3ab
AndriiTurianskyi <andriy.turyansky@gmail.com>
2020-12-04 23:45:40 +0200

    added initElem method

--- src/main/java/com/greencity/pages/components/NewsItemComponent.java
+++ src/main/java/com/greencity/pages/components/NewsItemComponent.java
@@ -1,14 +1,27 @@
 package com.greencity.pages.components;
 
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
+import java.util.List;
+
 public class NewsItemComponent {
-    WebDriver driver;
     WebElement img;
-    public NewsItemComponent(WebDriver driver){
-        this.driver=driver;
+    List<WebElement> categoryList;
+    WebElement title;
+    WebElement description;
+    WebElement createdDate;
+    WebElement author;
+
+    public NewsItemComponent(WebElement container) {
+
     }
 
-
+    private void initElem(WebElement container){
+        img= container.findElement(By.className("list-image-content"));
+        categoryList=container.findElements(By.className("ul-eco-buttons.ng-star-inserted"));
+        title=container.findElement(By.className("title-list.word-wrap"));
+        description=container.findElement(By.className("list-text.word-wrap"));
+       // createdDate=container.findElement(By.className("user-data-text-date"))
+    }
 }
