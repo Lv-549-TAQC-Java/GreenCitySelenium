6f0caece61f74267281c9f19b9f426516334de21
AndriiTurianskyi <andriy.turyansky@gmail.com>
2020-12-04 22:43:16 +0200

    fixed bugs

---
+++ src/main/java/com/greencity/pages/components/NewsItemComponent.java
@@ -0,0 +1,14 @@
+package com.greencity.pages.components;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class NewsItemComponent {
+    WebDriver driver;
+    WebElement img;
+    public NewsItemComponent(WebDriver driver){
+        this.driver=driver;
+    }
+
+
+}
