19339d168b122fde96ab34a2cd0dbdab0cbf8176
Khrystyna Pelekh <Khrystyna.Pelekh@outlook.com>
2020-12-08 00:00:06 +0200

    Edit create comment component

--- src/main/java/com/greencity/pages/components/CreateComment.java
+++ src/main/java/com/greencity/pages/components/CreateComment.java
@@ -1,9 +1,16 @@
 package com.greencity.pages.components;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
 public class CreateComment {
     WebElement imgAvatar;
     WebElement commentText;
     WebElement commentButton;
+
+    public void init(WebElement container){
+        imgAvatar = container.findElement(By.className("profile-avatar comments default-avatar ng-star-inserted"));
+        commentButton = container.findElement(By.className("primary-global-button"));
+        commentText = container.findElement(By.className("ng-touched ng-dirty ng-valid"));
+    }
 }
