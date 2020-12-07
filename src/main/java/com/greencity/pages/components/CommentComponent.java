2e92ebeb7b5bab7ff5f206cd80e70d27da47637c
Khrystyna Pelekh <Khrystyna.Pelekh@outlook.com>
2020-12-07 15:32:29 +0200

    Add comment component

---
+++ src/main/java/com/greencity/pages/components/CommentComponent.java
@@ -0,0 +1,32 @@
+package com.greencity.pages.components;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class CommentComponent {
+    WebElement imgAvatar;
+    WebElement authorName;
+    WebElement commentDate;
+    WebElement commentText;
+    WebElement editButton;
+    WebElement deleteButton;
+    WebElement replyButton;
+    WebElement commentLike;
+
+    public CommentComponent(WebElement container){
+        init(container);
+    }
+    public WebElement getImdAvatar(){return imgAvatar;}
+    public WebElement getAuthorName(){return authorName;}
+    public WebElement getCommentDate(){return commentDate;}
+    public WebElement getCommentText(){return commentText;}
+
+    private void init(WebElement container){
+        imgAvatar = container.findElement(By.cssSelector("div.comment-avatar"));
+        authorName = container.findElement(By.cssSelector("div.comment-details > div.author-name"));
+        commentDate = container.findElement(By.cssSelector("div.comment-details > div.comment-date-likes > " +
+                "div.comment-date"));
+        commentText = container.findElement(By.cssSelector("div.comment-main-text > p"));
+    }
+
+}
