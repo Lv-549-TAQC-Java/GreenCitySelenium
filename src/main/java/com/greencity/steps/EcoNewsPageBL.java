fe6b186d29b021ef77210421b054e7cdc00b1c3d
Yurii Krynytskyi <yuri.krynytsky@gmail.com>
2020-12-08 15:54:24 +0200

    krynytsky #10v8.12 Merged NewsItemComponentList with EcoNewsPage; Replaced business logic from EcoNewsPage to EcoNewsPageBL; Created ScrollPageDown util class; Created simple assert verifySearchOfTheList(); Deleted useless imports and spaces.

--- src/main/java/com/greencity/steps/EcoNewsPageBL.java
+++ src/main/java/com/greencity/steps/EcoNewsPageBL.java
@@ -1,17 +1,47 @@
 package com.greencity.steps;
 
 import com.greencity.pages.EcoNewsPage;
+import com.greencity.pages.components.NewsItemComponent;
+import com.greencity.utils.ScrollPageDown;
 import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
 
 public class EcoNewsPageBL {
     protected WebDriver driver;
     private EcoNewsPage ecoNewsPage;
-    public EcoNewsPageBL(WebDriver driver){
+
+    public EcoNewsPageBL(WebDriver driver) {
         this.driver = driver;
         ecoNewsPage = new EcoNewsPage(driver);
     }
 
-    public int numbOfNewsItemOnThePage() {
-        return ecoNewsPage.getNewsItemComponentList().getNumbOfNewsItemOnThePage();
+    public void verifySearchOfTheList() {
+        Assert.assertTrue(findListOfEcoNewsItem());
+    }
+
+    public boolean findListOfEcoNewsItem() {
+        new ScrollPageDown(driver).scrollToEndOfPage();
+        if (!(ecoNewsPage.getNewsList().size() > 0)) {
+            return false;
+        }
+        return true;
+    }
+
+    public int getNumbOfNewsItemOnThePage() {
+        return ecoNewsPage.getNewsList().size();
+    }
+
+    /* Further we can use this methods from here or create FindItemsPageBL*/
+    public NewsItemComponent findNewsItemByIndex(int index) {
+        return ecoNewsPage.getNewsList().get(index);
+    }
+
+    public NewsItemComponent findNewsItemByTitle(String title) {
+        for (NewsItemComponent newsItem : ecoNewsPage.getNewsList()) {
+            if (newsItem.getTitle().getText().equals(title)) {
+                return newsItem;
+            }
+        }
+        throw new RuntimeException("there is no news on the page with such a title-" + title);
     }
 }
