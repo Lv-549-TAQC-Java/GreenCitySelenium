8dc218d4f1ca1d1e6d1bc109dcfce5297207b8bf
Hanna Vasiunyk <annvasiunyk@gmail.com>
2020-12-08 19:25:22 +0200

    Deleted others elements

--- src/main/java/com/greencity/steps/CreateNewsPageBL.java
+++ src/main/java/com/greencity/steps/CreateNewsPageBL.java
@@ -1,20 +1,19 @@
 package com.greencity.steps;
 
 
-import com.greencity.pages.components.Tags;
+
 import org.openqa.selenium.WebDriver;
 
 public class CreateNewsPageBL {
     protected WebDriver driver;
-    private final Tags tags;
+
     public CreateNewsPageBL(WebDriver driver){
         this.driver = driver;
-        tags = Tags.getInstance(driver);
+
     }
 
-
     public CreateNewsPageBL clickOnTagCreateButton(){
-        tags.getNewsTag().click();
+
         return new CreateNewsPageBL(driver);
     }
 
