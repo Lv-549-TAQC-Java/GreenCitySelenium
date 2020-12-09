ff1bb18a7926992fbb37f83da005a3c2f1438a66
vadkostuk <vadkostuk@gmail.com>
2020-12-09 15:41:19 +0200

    added locators for NewsItemPage, changed methods for NewsItemPage, created new page element Link

--- src/main/java/com/greencity/pages/NewsItemPage.java
+++ src/main/java/com/greencity/pages/NewsItemPage.java
@@ -1,70 +1,58 @@
 package com.greencity.pages;
 
+import com.greencity.locators.NewsItemPageLocators;
+import com.greencity.pageelements.Button;
+import com.greencity.pageelements.Image;
+import com.greencity.pageelements.Link;
+import com.greencity.pageelements.TextField;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 public class NewsItemPage extends BasePage{
-    private WebElement backToNewsButton;
-    private WebElement itemCategoryTag;
-    private WebElement newsTitle;
-    private WebElement dateInfo;
-    private WebElement authorInfo;
-    private WebElement image;
-    private WebElement textContent;
-    private WebElement sourceField;
-    private WebElement socialNetworkingLink;
-    private WebElement mayBeInterestingForYouNewsItem;
+    private Button backToNewsButton;
+    private TextField itemCategoryTag;
+    private TextField newsTitle;
+    private TextField dateInfo;
+    private TextField authorInfo;
+    private Image image;
+    private TextField textContent;
+    private Link sourceLink;
+
 
     public NewsItemPage(WebDriver driver) {
         super(driver);
     }
-    public WebElement getBackToNewsButton() {
-        return backToNewsButton = driver
-                .findElement(By.cssSelector(".button-link > .button-content"));
+    public Button getBackToNewsButton() {
+        return backToNewsButton = new Button(driver, NewsItemPageLocators.BACK_TO_NEWS_BUTTON);
     }
 
-    public WebElement getItemCategoryTag() {
-        return itemCategoryTag = driver
-                .findElement(By.cssSelector(".tags"));
+    public TextField getItemCategoryTag() {
+        return itemCategoryTag =new TextField(driver,NewsItemPageLocators.ITEM_CATEGORY_TAG);
     }
-    public WebElement getNewsTitle(){
-        return newsTitle = driver
-                .findElement(By.cssSelector(".news-title-container"));
+    public TextField getNewsTitle(){
+        return newsTitle = new TextField(driver,NewsItemPageLocators.NEWS_TITLE);
     }
 
-    public WebElement getDateInfo() {
-        return dateInfo= driver
-                .findElement(By.cssSelector(".news-info-date"));
+    public TextField getDateInfo() {
+        return dateInfo= new TextField(driver,NewsItemPageLocators.DATE_INFO);
     }
 
-    public WebElement getAuthorInfo() {
-        return authorInfo = driver
-                .findElement(By.cssSelector(".news-info-author"));
+    public TextField getAuthorInfo() {
+        return authorInfo = new TextField(driver,NewsItemPageLocators.AUTHOR_INFO);
     }
 
-    public WebElement getImage() {
-        return image = driver
-                .findElement(By.cssSelector("img[alt='news-image']"));
+    public Image getImage() {
+        return image =new Image(driver,NewsItemPageLocators.IMAGE);
     }
 
-    public WebElement getTextContent() {
-        return textContent = driver
-                .findElement(By.cssSelector("img[alt='news-image']"));
+    public TextField getTextContent() {
+        return textContent =new TextField(driver,NewsItemPageLocators.TEXT_CONTENT);
     }
 
-    public WebElement getSourceField() {
-        return sourceField = driver
-                .findElement(By.cssSelector(".source-text.word-wrap"));
+    public Link getSourceField() {
+        return sourceLink = new Link(driver,NewsItemPageLocators.SOURCE_LINK);
     }
 
-    public WebElement getSocialNetworkingLink(int chooseSocialNetworkLink) {
-        return socialNetworkingLink = driver
-                .findElement(By.cssSelector(".news-links-images > a:nth-of-type("+chooseSocialNetworkLink+")"));
-    }
 
-    public WebElement getMayBeInterestingForYouNewsItem(int chooseNewsItem) {
-        return mayBeInterestingForYouNewsItem = driver
-                .findElement(By.cssSelector(".list > li:nth-of-type("+chooseNewsItem+")"));
-    }
 }
