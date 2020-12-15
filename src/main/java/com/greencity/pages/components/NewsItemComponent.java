aacb1e26119515938dd03d918a24302b844480c0
Yurii Krynytskyi <yuri.krynytsky@gmail.com>
2020-12-15 12:23:48 +0200

    krynytsky #31 Updated NewsItemComponent

--- src/main/java/com/greencity/pages/components/NewsItemComponent.java
+++ src/main/java/com/greencity/pages/components/NewsItemComponent.java
@@ -6,53 +6,33 @@ import org.openqa.selenium.WebElement;
 import java.util.List;
 
 public class NewsItemComponent {
-    private WebElement img;
-    private List<WebElement> categoryList;
-    private WebElement title;
-    private WebElement description;
-    private WebElement createdDate;
-    private WebElement author;
     private final WebElement container;
+
     public NewsItemComponent(WebElement container) {
         this.container=container;
-        initElem();
     }
 
     public WebElement getImg() {
-        return img;
+        return this.container.findElement(By.cssSelector("div.list-image"));
     }
 
     public List<WebElement> getCategoryList() {
-        return categoryList;
+        return this.container.findElements(By.cssSelector("div.filter-tag > div"));
     }
 
     public WebElement getTitle() {
-        return title;
+        return this.container.findElement(By.cssSelector("div.title-list"));
     }
 
     public WebElement getDescription() {
-        return description;
+        return this.container.findElement(By.cssSelector("div.list-text"));
     }
 
-    public WebElement getCreatedDate() {
-        return createdDate;
-    }
-
-    public WebElement getAuthor() {
-        return author;
+    public List<WebElement> getUserDateList(){
+        return this.container.findElements(By.cssSelector("div.user-data-added-news > p"));
     }
 
     public void click(){
        container.click();
-    }
-
-    private void initElem() {
-        img = container.findElement(By.cssSelector("div.list-image"));
-        categoryList = container.findElements(By.cssSelector("div.filter-tag > div"));
-        title = container.findElement(By.cssSelector("div.title-list"));
-        description = container.findElement(By.cssSelector("div.list-text"));
-        List<WebElement> userDataList = container.findElements(By.cssSelector("div.user-data-added-news > p"));
-        createdDate = userDataList.get(0);
-        author = userDataList.get(1);
     }
 }
